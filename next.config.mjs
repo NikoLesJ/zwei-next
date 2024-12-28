223a09c2cdd6719cc8cbfff3445a38bd7f0f65a4
NikoLesJ <3gdoma@gmail.com>
2024-12-28 21:12:01 +0100

    klein upd

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    async headers() {
+      return [
+        {
+          source: "/(.*)", // применить ко всем маршрутам
+          headers: [
+            {
+              key: "X-Robots-Tag",
+              value: "noindex, nofollow",
+            },
+          ],
+        },
+      ];
+    },
+  };
+  
+  module.exports = nextConfig;
+  
